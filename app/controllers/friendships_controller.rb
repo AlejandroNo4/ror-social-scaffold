0928a42ae58001568ed8aa2fa91025e2bd9111ef
Alejandro Contreras <kdta15@outlook.com>
2021-04-19 12:07:56 -0500

    Fix rubocop linters

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -11,15 +11,18 @@ class FriendshipsController < ApplicationController
 
   def update
     requester = User.find params[:user_id]
-    if current_user.confirm_friend(requester, current_user)
-      flash[:notice] = 'Request accepted!'
-      redirect_to request.referrer
-    end
+    flash[:notice] = if current_user.confirm_friend(requester, current_user)
+                       'Request accepted!'
+                     else
+                       'Request denied!'
+                     end
+    redirect_to request.referrer
   end
 
   def destroy
     @other_user = User.find params[:id]
-    @f_to_del = current_user.requested_friendships.where(requester_id: current_user.id) + @other_user.requested_friendships.where(requester_id: @other_user.id)
+    @f_to_del = current_user.requested_friendships.where(requester_id: current_user.id) +
+                @other_user.requested_friendships.where(requester_id: @other_user.id)
     @f_to_del.each(&:destroy)
     flash[:notice] = 'Deleted succesfully'
     redirect_to request.referrer
